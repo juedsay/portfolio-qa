51512b4ae4379222f9d9aaa722f3900bd4580680
Julian Simon <juedsay@gmail.com>
2025-01-08 19:03:50 -0300

    deploy website

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,4 @@ import robotsTxt from "astro-robots-txt"
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://juedsay.github.io',
-  base: '/portfolio-qa/',
 })
