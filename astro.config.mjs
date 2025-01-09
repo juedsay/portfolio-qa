843438ca3f9376d6085a2e38f974b731401f8185
Julian Simon <juedsay@gmail.com>
2025-01-09 02:38:25 -0300

    correcion de rutas de imagenes para produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juedsay.github.io',
+  base: '/portfolio-qa/',
   integrations: [tailwind(), robotsTxt()],
 })
